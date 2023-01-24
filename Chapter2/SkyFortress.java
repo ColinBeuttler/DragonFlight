c9a614f977b18b92b0cdfc593f8dade515d41494
Colin Beuttler <beutt102@mail.chapman.edu>
2023-01-24 11:24:30 -0800

    Skyfortress1

--- Chapter2/SkyFortress.java
+++ Chapter2/SkyFortress.java
@@ -8,23 +8,24 @@ import Dialogue.Fillers;
 public class SkyFortress {
     
     public static void localeIntro(){
-        System.out.println("You find yourself");
+        System.out.println("You look ou tinto a giant black horizon...");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("its seems that you are on the edge of space, you can see the stars more clearly than ever before.");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("You then realize that you had stepped on a large metal platform that is moving upward...");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("suddenly it stops and in front of you is a giantic metal structure.");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("The structure seems to be made of various smaller pieces of sheet metal. ");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("most of them painted vibrant colors...");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("there are even small pieces of glass, which you assume are makeshift windows.");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("As you move closer to this giant metal object, you notice a structure jutting out in the front...");
         Fillers.wait(2);
-        System.out.println("");
+        System.out.println("it appears to be a separate piece than the rest of the structure that had been attached later.");
         Fillers.wait(2);
+        System.out.println("The structure has a small colored glass window on the front with two large bright eyes peering through it...");
     }
 }
