d48bc2b6d40d64ad31f671d1cefad00267ba2d77
Colin Beuttler <beutt102@mail.chapman.edu>
2023-01-25 14:22:28 -0800

    removebloodcave

--- Chapter2/DeepWaterMountains.java
+++ Chapter2/DeepWaterMountains.java
@@ -20,5 +20,13 @@ public class DeepWaterMountains {
         System.out.println("You can now see that the runoff was leading to a small lake within this large chamber.");
         Fillers.wait(2);
         System.out.println("Next to the lake lies a small city, you head down the narrow path towards the city...");
+        Fillers.wait(2);
+        System.out.println("some of the buildings within the city lie directly on the lake while others are carved directly into the stalagmites");
+        Fillers.wait(2);
+        System.out.println("As you start to get closer to the city you hear a small crunch underneath your feet...");
+        Fillers.wait(2);
+        System.out.println("you look down to see the floor of the chamber around you is littered with small bones.");
+        Fillers.wait(2);
+        System.out.println("the ceiling seems to be made of either fur and scales and occasionly you notice a slight shifting.");
     }
 }
